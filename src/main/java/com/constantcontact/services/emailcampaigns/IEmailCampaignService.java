6828a16219a770e9a839af5a90857bf435460da7
Halus Stefan <stefan.halus@osf-global.com>
2015-08-18 13:30:38 +0300

    EmailCampaignService - Added pagination implementation + tests.

--- src/main/java/com/constantcontact/services/emailcampaigns/IEmailCampaignService.java
+++ src/main/java/com/constantcontact/services/emailcampaigns/IEmailCampaignService.java
@@ -2,6 +2,7 @@ package com.constantcontact.services.emailcampaigns;
 
 import com.constantcontact.components.emailcampaigns.EmailCampaignRequest;
 import com.constantcontact.components.emailcampaigns.EmailCampaignResponse;
+import com.constantcontact.components.generic.response.Pagination;
 import com.constantcontact.components.generic.response.ResultSet;
 import com.constantcontact.exceptions.service.ConstantContactServiceException;
 import com.constantcontact.services.base.IBaseService;
@@ -17,7 +18,7 @@ public interface IEmailCampaignService extends IBaseService {
 	/**
 	 * Gets all the Email Campaigns.<br/>
 	 * Implements the get Campaigns operation of the Email Campaign API by calling the ConstantContact server side.
-	 * 
+	 *
 	 * @param limit The limit
 	 * @param modifiedSinceTimestamp This time stamp is an ISO-8601 ordinal date supporting offset. It will return only the Email Campaigns modified since the supplied date.
 	 * @return A {@link ResultSet} of {@link EmailCampaignResponse} containing data as returned by the server on success; <br/>
@@ -25,6 +26,16 @@ public interface IEmailCampaignService extends IBaseService {
 	 * @throws ConstantContactServiceException When something went wrong in the Constant Contact flow or an error is returned from server.
 	 */
 	ResultSet<EmailCampaignResponse> getCampaigns(Integer limit, String modifiedSinceTimestamp) throws ConstantContactServiceException;
+
+    /**
+     * Gets all the Email Campaigns.<br/>
+     * Implements the get Campaigns operation of the Email Campaign API by calling the ConstantContact server side.
+     *
+     * @param pagination A {@link Pagination} instance containing the link to the next page of results.
+     *                   An exception is thrown otherwise.
+     * @throws ConstantContactServiceException When something went wrong in the Constant Contact flow or an error is returned from server.
+     */
+    ResultSet<EmailCampaignResponse> getCampaigns(Pagination pagination) throws ConstantContactServiceException;
 	
 	/**
 	 * Gets a single Email Campaign.<br/>
