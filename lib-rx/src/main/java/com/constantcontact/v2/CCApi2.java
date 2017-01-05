70289aab383e10c5e5fd3f9def5e9d3f0f8dee10
Gyuri Grell <ggrell@constantcontact.com>
2017-01-05 15:24:21 -0500

    Added custom converters for posting Campaign objects.

--- lib-rx/src/main/java/com/constantcontact/v2/CCApi2.java
+++ lib-rx/src/main/java/com/constantcontact/v2/CCApi2.java
@@ -1,11 +1,11 @@
 package com.constantcontact.v2;
 
+import com.constantcontact.v2.converter.jackson.JacksonConverterFactory;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.jackson.JacksonConverterFactory;
 
 import java.util.concurrent.TimeUnit;
 
