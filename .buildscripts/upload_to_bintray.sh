4321dba67b2d317ceb3accaeb65f18c6459d54a1
Gyuri Grell <ggrell@pobox.com>
2018-03-09 11:16:15 -0500

    Corrected bintray key inputs

--- .buildscripts/upload_to_bintray.sh
+++ .buildscripts/upload_to_bintray.sh
@@ -4,8 +4,6 @@ SLUG="constantcontact/java-sdk"
 JDK="oraclejdk8"
 BRANCH="master"
 
-set
-
 set -e
 if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
   echo "Skipping upload: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
@@ -15,6 +13,10 @@ elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "Skipping upload: was pull request."
 elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
   echo "Skipping upload: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+elif [ -z "$BINTRAY_USER" ]; then
+  echo "Skipping upload: Expected BINTRAY_USER to be set."
+elif [ -z "$BINTRAY_KEY" ]; then
+  echo "Skipping upload: Expected BINTRAY_KEY to be set."
 else
   echo "Uploading build to Bintray..."
   ./gradlew clean bintrayUpload
