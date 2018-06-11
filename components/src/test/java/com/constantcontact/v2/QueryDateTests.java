de371cd67dd9a4a2cbb7db5e3dfa31c734723cb5
mdavila <maria.davila@endurance.com>
2018-06-11 07:37:56 -0400

    MOB-5721 use full format for ISO-8601 dates, with "Z" at the end

--- components/src/test/java/com/constantcontact/v2/QueryDateTests.java
+++ components/src/test/java/com/constantcontact/v2/QueryDateTests.java
@@ -12,8 +12,8 @@ public class QueryDateTests {
 
     @Test
     public void test24HourDateConversions() {
-        ZonedDateTime date = ZonedDateTime.parse("2018-06-04T09:07:30-04:00");
+        ZonedDateTime date = ZonedDateTime.parse("2018-06-04T09:07:30-04:00Z");
         QueryDate qd = new QueryDate(date.toInstant().toEpochMilli());
-        assertThat(qd.toString().indexOf("2018-06-04T13:07:30"), not(equalTo(-1)));
+        assertThat(qd.toString().indexOf("2018-06-04T13:07:30:00Z"), not(equalTo(-1)));
     }
 }
